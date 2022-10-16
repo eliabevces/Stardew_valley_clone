197408748d09fe8b3dd7acf82a3655199e17c037
Eliabevces <eliabevces@ufu.br>
2022-10-16 11:57:06 -0300

    Importing player graphics

--- code/player.py
+++ code/player.py
@@ -1,49 +1,66 @@
 import pygame
+import os
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, group):
-        super().__init__(group)
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('blue')
-        self.rect = self.image.get_rect(center = pos)
+  def __init__(self, pos, group):
+    super().__init__(group)
 
-        # movement atrributes
-        self.direction = pygame.math.Vector2()
-        self.pos = pygame.math.Vector2(self.rect.center)
-        self.speed = 200
+    self.import_assets()
+    self.status = "down_idle"
+    self.frame_index = 0
 
-    def input(self):
-      keys = pygame.key.get_pressed()
+    # general setup
+    self.image = self.animations[self.status][self.frame_index]
+    self.rect = self.image.get_rect(center = pos)
 
-      if keys[pygame.K_UP]:
-        self.direction.y = -1
-      elif keys[pygame.K_DOWN]:
-        self.direction.y = 1
-      else:
-        self.direction.y = 0
-      
-      if keys[pygame.K_LEFT]:
-        self.direction.x = -1
-      elif keys[pygame.K_RIGHT]:
-        self.direction.x = 1
-      else:
-        self.direction.x = 0
-  
-    def move(self, dt):
+    # movement atrributes
+    self.direction = pygame.math.Vector2()
+    self.pos = pygame.math.Vector2(self.rect.center)
+    self.speed = 200
 
-      # normalizing a vector
-      if self.direction.magnitude() > 0:
-        self.direction = self.direction.normalize()
+  def import_assets(self):
+    self.animations = {'up' : [], 'down' : [], 'left' : [], 'right' : [],
+                      'right_idle' : [], 'left_idle' : [], 'up_idle' : [], 'down_idle' : [],
+                      'right_hoe' : [], 'left_hoe' : [], 'up_hoe' : [], 'down_hoe' : [],
+                      'right_axe' : [], 'left_axe' : [], 'up_axe' : [], 'down_axe' : [],
+                      'right_water' : [], 'left_water' : [], 'up_water' : [], 'down_water' : []}
+    for animation in self.animations.keys():
+      full_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'graphics', 'character\\') + animation
+      self.animations[animation] = import_folder(full_path)
 
-      # horizontal movement
-      self.pos.x += self.direction.x * self.speed * dt
-      self.rect.centerx = self.pos.x
+  def input(self):
+    keys = pygame.key.get_pressed()
 
-      # vertical movement
-      self.pos.y += self.direction.y * self.speed * dt
-      self.rect.centery = self.pos.y
+    if keys[pygame.K_UP]:
+      self.direction.y = -1
+    elif keys[pygame.K_DOWN]:
+      self.direction.y = 1
+    else:
+      self.direction.y = 0
+    
+    if keys[pygame.K_LEFT]:
+      self.direction.x = -1
+    elif keys[pygame.K_RIGHT]:
+      self.direction.x = 1
+    else:
+      self.direction.x = 0
 
-    def update(self, dt):
-      self.input()
-      self.move(dt)
+  def move(self, dt):
+
+    # normalizing a vector
+    if self.direction.magnitude() > 0:
+      self.direction = self.direction.normalize()
+
+    # horizontal movement
+    self.pos.x += self.direction.x * self.speed * dt
+    self.rect.centerx = self.pos.x
+
+    # vertical movement
+    self.pos.y += self.direction.y * self.speed * dt
+    self.rect.centery = self.pos.y
+
+  def update(self, dt):
+    self.input()
+    self.move(dt)
